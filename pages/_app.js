419543afa065a1b722363d963bc648ade5dfe5fd
Bright Iortsor <iortsor@gmail.com>
2023-03-22 14:01:50 +0100

    added page Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,12 @@
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
+
+export default MyApp;
