89f525b9955d90c30e5d6e3ccda2d7ac95b1cc3b
Bright Iortsor <iortsor@gmail.com>
2023-03-27 11:31:34 +0100

    added useEffect hook to handle price changes

--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,16 +1,35 @@
 import styles from "../../styles/Product.module.css";
 import Image from "next/image";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Product = () => {
   const [size, setSize] = useState(0);
+  const [ingredients, setIngredients] = useState([]);
+
   const pizza = {
     id: 1,
     img: "/images/pizza.webp",
     name: "CAMPAGNOLA",
-    price: [19.9, 23.9, 27.9],
+    price: [3500, 5000, 7500],
     desc: "Campagnola is a classic Italian pizza with a tomato sauce, mozzarella, and fresh basil. It is a simple pizza that is perfect for those who like a simple, yet delicious pizza.",
   };
+
+  const handleIngredients = (event) => {
+    const ingredient = event.target.name;
+    if (event.target.checked) {
+      setIngredients([...ingredients, ingredient]);
+    } else {
+      setIngredients(ingredients.filter((i) => i !== ingredient));
+    }
+  };
+
+  useEffect(() => {
+    const extraIngredientsPrice = ingredients.length * 1000;
+    const newPrice = pizza.price[size] + extraIngredientsPrice;
+    setPizzaPrice(newPrice);
+  }, [ingredients, size]);
+
+  const [pizzaPrice, setPizzaPrice] = useState(pizza.price[size]);
 
   return (
     <div className={styles.container}>
@@ -27,7 +46,10 @@ const Product = () => {
       </div>
       <div className={styles.right}>
         <h1 className={styles.title}>{pizza.name}</h1>
-        <span className={styles.price}>${pizza.price[size]}</span>
+        <span className={styles.price}>
+          <span className={styles.naira}>N</span>
+          {pizzaPrice}
+        </span>
         <p className={styles.desc}>{pizza.desc}</p>
         <h3 className={styles.choose}>Choose the size</h3>
         <div className={styles.sizes}>
@@ -52,6 +74,7 @@ const Product = () => {
               id="double"
               name="double"
               className={styles.checkbox}
+              onChange={handleIngredients}
             />
             <label htmlFor="double">Double Ingredients</label>
           </div>
@@ -61,6 +84,7 @@ const Product = () => {
               type="checkbox"
               id="cheese"
               name="cheese"
+              onChange={handleIngredients}
             />
             <label htmlFor="cheese">Extra Cheese</label>
           </div>
@@ -70,6 +94,7 @@ const Product = () => {
               type="checkbox"
               id="spicy"
               name="spicy"
+              onChange={handleIngredients}
             />
             <label htmlFor="spicy">Spicy Sauce</label>
           </div>
@@ -79,6 +104,7 @@ const Product = () => {
               type="checkbox"
               id="garlic"
               name="garlic"
+              onChange={handleIngredients}
             />
             <label htmlFor="garlic">Garlic Sauce</label>
           </div>
